8727cb96bb59a1a665813a1137d7301381f89ce6
이동현 <idonghyeon@idonghyeon-ui-MacBookPro.local>
2024-10-03 01:50:57 +0900

    initial commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -21,19 +21,16 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
-  // eslint-disable-next-line no-var
   var __mongoClientPromise: Promise<MongoClient>;
 }
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value is preserved across module reloads
   if (!global.__mongoClientPromise) {
     client = new MongoClient(uri);
     global.__mongoClientPromise = client.connect();
   }
   clientPromise = global.__mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
